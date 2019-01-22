045c6bd3fe8627158b06871caeb9fccc77b47c4d
Chandan Kumar Ojha <mr.chandanojha@gmail.com>
2019-01-22 14:37:01 +0530

    added spider global desi

---
+++ crawler_nykaa/spiders/globaldesi.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.http.request import Request
+from scrapy.spider import SitemapSpider
+import json
+
+
+class GlobalDesiSpider(SitemapSpider):
+    name = 'globaldesi'
+    allowed_domains = ['globaldesi.in']
+    sitemap_urls = ['https://www.globaldesi.in/sitemap.xml']
+    # start_urls = [
+    #     'https://www.globaldesi.in/sale-products/suits/black-printed-co-ordinates-62417.html'
+    # ]
+
+    def parse(self, response):
+        pattern = r"webengage\.track\(\"GD\sProducts\sViews\",\s({.*})\);"
+        try:
+            txt = response.xpath("normalize-space(.//script[contains(.,'GD Products Views')]/text())").re_first(pattern)
+            data = eval(txt)
+            # print(data)
+        except Exception as e:
+            print("exception raised:", e)
+            data = {}
+        if "Product Ids" in data:
+            id_text = "discount-percentage-{}".format(data["Product Ids"])
+            discount = response.xpath(".//span[@id='" + id_text + "']/text()").extract_first()
+            data['discount'] = discount
+            yield data
+
+        page_links = response.xpath(".//div[@class='pages']/ol/li/a/@href").extract()
+        if page_links:
+            for link in page_links:
+                print(link)
+                yield Request(link, callback=self.parse)
